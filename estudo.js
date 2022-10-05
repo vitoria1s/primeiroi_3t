09a292f3f303e79a8acda6ff62525a64608d542d
vitoria1s <110063364+vitoria1s@users.noreply.github.com>
2022-10-05 16:58:34 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,19 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Terceiro Trimestre</title>
-    <script src="estudo.js"></script>
+// alert("Oi! Eu sou o Alert");
+// document.write("Estou na página.");
+// console.log("Eu estou no console.");
+let tabuada = 5;
+function alo(){
 
-</head>
-<body
+    let tabuada = document.getElementById("entrada").value;
 
-Valor: <input id="valor" type="text"> <br>
-Taxa de juros: <input id="juros" type="text"> <br>
-<button onclick="total()">Enviar</button>
+    document.write("<h1>Tabuada do " + tabuada + "</h1>");
+    document.write(tabuada + " x 1 = " + (tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10));
+}
+// for(inicio,validacao,manutencao)
 
-</body>  
-</html>
+function total(){
+    let val = document.getElementById("valor").value;
+    let ju = document.getElementById("juros").value;
+    let resultado = (val * (ju/100)+1) + val;
+    document,write("O total e de: " + resultado);
+}
 
